19f082cb58ad336b93c7e37427261e5bf2d617e2
PattyB <patty_eb18@hotmail.com>
2020-05-12 16:31:21 -0600

    eliminar routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -11,7 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,HttpClientModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
